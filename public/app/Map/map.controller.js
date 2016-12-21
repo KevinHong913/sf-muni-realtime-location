17cb45b494498f478ad451aa74215e091c250d50
Kevin Hong <kevin.hong@sap.com>
2016-12-20 16:46:21 -0800

    latest version

--- public/app/Map/map.controller.js
+++ public/app/Map/map.controller.js
@@ -11,18 +11,30 @@ angular.module('sfmuni.map')
 		/**** VARIABLES ****/
 		vm.mercatorProjection = d3.geoMercator()
 		  .center([-122.44, 37.78])
-		  .scale(330000);
+		  .scale(350000);
 		vm.geoPath = d3.geoPath().projection( vm.mercatorProjection );
 		vm.routeList = [];
-		vm.routeConfig;
-		vm.vehicleLocation;
-		vm.selectedRoute = "N";
+		vm.routeConfig = [];
+		vm.vehicleLocation = [];
+		vm.selectedRoute = ["N"];
 		vm.currentVehicle;
 
 		vm.initialize = function() {
 			console.log("MAP CONTROLLER INIT");
 			vm.getRouteList();
 		};
+
+		vm.addNewRoute = function() {
+			if(vm.selectedRoute.length <= 5) {
+				vm.selectedRoute.push("");
+			}
+		}
+
+		vm.deleteRoute = function(index) {
+			if(vm.selectedRoute.length > 1) {
+				vm.selectedRoute.splice(index, 1)
+			}
+		}
 
 		vm.setCurrentVehicle = function(data) {
 			vm.currentVehicle = data;
@@ -42,49 +54,7 @@ angular.module('sfmuni.map')
 	    });
 		}
 
-		// Get certain route's config data
-		vm.getRouteConfig = function(routeName) {
-			NextBusService.GetRouteConfig(routeName)
-			.then( function(response) {
-				console.log("Get Route Config", response);
-				if(response.data.body.route) {
-					vm.routeConfig = response.data.body.route;
-				}
-			}), function(error) {
-				console.log("Error getting data of file " + fileName, error);
-			}
-		}
-
-		// Get certain route's all vehicles' location
-		vm.getVehicleLocation = function(routeName) {
-			NextBusService.GetVehicleLocation(routeName)
-			.then( function(response) {
-				console.log("Get Vehicle Location", response);
-				if(response.data.body.vehicle) {
-					var vehicles = response.data.body.vehicle;
-					// sometimes the response type will be different...
-					// not only need to check if is object but need to check if it is equal to itself,
-					// because null is also consider as object in javascript
-					if(typeof vehicles === 'object' && !(vehicles == vehicles) ) {
-						vm.vehicleLocation = [ vehicles ];
-					} else if(Array.isArray(vehicles)) {
-						vm.vehicleLocation = vehicles;
-					} else {
-						vm.vehicleLocation = []; // empty array to at least trigger watch event to clean point
-					}
-				}
-			}), function(error) {
-				console.log("Error getting data of file " + fileName, error);
-			}
-		}
-
 		vm.initialize();
-
-		// Refresh vehicle location every 15 second
-		$interval(function(){
-			console.log("[REFRESH]")
-			vm.getVehicleLocation(vm.selectedRoute);
-		}, 15000);
 
 	};
 
@@ -104,12 +74,29 @@ angular.module('sfmuni.map')
 				.style('width', width)
 				.style('height', height)
 
+				var zoomBehavior = d3.zoom()
+		        .scaleExtent([0.8, 3])
+		        .on("zoom", zoomed);
+
 				// map d3 element
 				var neighborhoods = svg.append( "g" );
 				var streets = svg.append( "g" );
 				var freeways = svg.append( "g" );
 				var arteries = svg.append( "g" );
 				var neighborhoodsOutline = svg.append("g");
+
+				var zoomCover = svg.append("rect");
+
+				zoomCover.style("width", "100%")
+				.attr("class", "zoomCover")
+		    .style("height", "100%")
+		    .attr("fill", "none")
+		    .style("pointer-events", "all")
+		    .call(zoomBehavior);
+
+		    var zoomed = function() {
+				  svg.selectAll("g").attr("transform", d3.event.transform);
+				}
 
 				// draw map (this will only run once)
 				var renderMap = function() {
@@ -156,68 +143,83 @@ angular.module('sfmuni.map')
 }])
 
 // render route, stops, and vehicle locations
-.directive('d3MapRoute', ['SfMapData', function(SfMapData){
+.directive('d3MapRoute', ['SfMapData', 'NextBus', '$interval', function(SfMapData, NextBusService, $interval){
 	return {
 		require: '^d3Map',
 		scope: {
-			routeConfig: '=',
-			vehicleLocation: '=',
-			selectedRoute: '='
+			route: '=',
+			index: '='
 		},
 		restrict: 'EA',
 		link: {
 			pre: function($scope, iElm, iAttrs, ctrl) {
-				var margin = parseInt(iAttrs.margin) || 20;
 
+				// Get certain route's config data
+				var getRouteConfig = function(routeName) {
+					NextBusService.GetRouteConfig(routeName)
+					.then( function(response) {
+						console.log("Get Route Config", response);
+						if(response.data.body.route) {
+							$scope.routeConfig = response.data.body.route;
+						}
+					}), function(error) {
+						console.log("Error getting data of file " + fileName, error);
+					}
+				}
+
+				// Get certain route's all vehicles' location
+				var getVehicleLocation = function(routeName) {
+					NextBusService.GetVehicleLocation(routeName)
+					.then( function(response) {
+						console.log("Get Vehicle Location", response);
+						if(response.data.body.vehicle) {
+							var vehicles = response.data.body.vehicle;
+							// sometimes the response type will be different...
+							// not only need to check if is object but need to check if it is equal to itself,
+							// because null is also consider as object in javascript
+							if(typeof vehicles === 'object' && !(vehicles == vehicles) ) {
+								$scope.vehicleLocation = [ vehicles ];
+							} else if(Array.isArray(vehicles)) {
+								$scope.vehicleLocation = vehicles;
+							} else {
+								$scope.vehicleLocation = []; // empty array to at least trigger watch event to clean point
+							}
+						}
+					}), function(error) {
+						console.log("Error getting data of file " + fileName, error);
+					}
+
+				}
+
+				var initialize = function() {
+					console.log("d3 map route init");
+
+					if($scope.route) {
+						getRouteConfig($scope.route);
+						getVehicleLocation($scope.route);
+					}
+				}
+
+				initialize();
 				var width = $(svg).width();
 				var hegiht = $(svg).height();
 
 				var svg = d3.select(iElm[0].parentNode).select("svg");
-				var selectedRoute = ctrl.selectedRoute;
-
-		    var zoomed = function() {
-				  svg.selectAll("g").attr("transform", d3.event.transform);
-				}
-
-				var zoomBehavior = d3.zoom()
-		        .scaleExtent([0.8, 3])
-		        .on("zoom", zoomed);
-
 				var routeLines = svg.append("g");
-				var zoomCover = svg.append("rect");
 				var stops = svg.append("g");
 				var vehicles = svg.append("g");
+
 
 				// Define the div for the tooltip
 				var div = d3.select("body").append("div")
 				    .attr("class", "tooltip")
 				    .style("opacity", 0);
 
-				zoomCover.style("width", "100%")
-		    .style("height", "100%")
-		    .attr("fill", "none")
-		    .style("pointer-events", "all")
-		    .call(zoomBehavior);
-
-
-				$scope.$watch( 'selectedRoute', function(newVal, oldVal) {
-					selectedRoute = newVal;
-					ctrl.getRouteConfig(newVal);
-				})
-
-				$scope.$watchCollection( 'routeConfig', function(newVal, oldVal) {
-	  			$scope.drawRoute(newVal);
-	  			$scope.drawStop(newVal);
-				});
-
-				$scope.$watchCollection( 'vehicleLocation', function(newVal, oldVal) {
-	  			 $scope.drawVehicle(newVal);
-				});
 
 				// Draw route
 				$scope.drawRoute = function(data) {
 					if(!data) return;
-					svg.selectAll('.routeLine').remove();
+					svg.selectAll('.routeLine.route' + $scope.index).remove();
 
 					// create GeoJson format object
 					var routeLine = [];
@@ -233,22 +235,22 @@ angular.module('sfmuni.map')
 					routeLines.selectAll('path')
 						.data(routeLine)
 						.enter().append("path")
-						.attr( "class", "routeLine")
+						.attr( "class", "routeLine route" + $scope.index)
             .attr("d", ctrl.geoPath )
 
-           ctrl.getVehicleLocation(selectedRoute);
+           // ctrl.getVehicleLocation($scope.route);
 				}
 
 				// Draw Stops
 				$scope.drawStop = function(data) {
 					if(!data) return;
 
-					svg.selectAll(".stop").remove();
+					svg.selectAll(".stop.route" + $scope.index).remove();
 
-					stops.selectAll(".stop")
+					stops.selectAll(".stop.route" + $scope.index)
 					.data(data.stop)
 					.enter().append("circle")
-					.attr("class", "stop")
+					.attr("class", "stop route" + $scope.index)
 					.attr("cx", function(d) {
 					       return ctrl.mercatorProjection([d._lon, d._lat])[0];
 					})
@@ -265,27 +267,28 @@ angular.module('sfmuni.map')
 				$scope.drawVehicle = function(data) {
 					if(!data) return;
 					// console.log("data", data);
-					var tmp = svg.selectAll('.vehicle').remove();
+					var tmp = svg.selectAll('.vehicle.route' + $scope.index).remove();
 
-					vehicles.selectAll(".vehicle")
+					vehicles.selectAll(".vehicle.route" + $scope.index)
 					.data(data)
 					.enter().append("rect")
-					.attr("class", "vehicle")
+					.attr("class", "vehicle route" + $scope.index)
 					.attr("transform", function(d) {
 						// Calculate 2D point rotation then apply to translate
-						var radium = d._heading * Math.PI / 180;
+						var radium = (d._heading - 90) * Math.PI / 180;
 						var newX = 6 * Math.cos(radium) - 3 * Math.sin(radium);
 						var newY = 3 * Math.cos(radium) + 6 * Math.sin(radium);
 						var x = ctrl.mercatorProjection([d._lon, d._lat])[0] - newX;
 						var y = ctrl.mercatorProjection([d._lon, d._lat])[1] - newY;
 
-						return "translate(" + x + ", " + y + ")rotate(" + d._heading + ")"
+						return "translate(" + x + ", " + y + ")rotate(" + (d._heading -90) + ")"
 					})
 					.on('mouseover', vehicleMouseOver)
 					.on('mouseout', vehicleMouseOut)
 
 				}
 
+				/**** EVENT HANDLER ****/
 				var vehicleMouseOver = function(d, i) {
 					d3.select(this)
 					.attr('width', 15)
@@ -330,6 +333,32 @@ angular.module('sfmuni.map')
 				  .duration(500)
 				  .style("opacity", 0);
 				}
+
+				// Refresh vehicle location every 15 second
+				$interval(function(){
+					console.log("[REFRESH]")
+
+					if($scope.route) {
+						getVehicleLocation($scope.route);
+					}
+				}, 15000);
+
+				/**** WATCH FUNCTIONS *****/
+				$scope.$watchCollection( 'route', function(newVal, oldVal) {
+					if(newVal) {
+						getRouteConfig(newVal);
+						getVehicleLocation(newVal);
+					}
+				})
+
+				$scope.$watchCollection( 'routeConfig', function(newVal, oldVal) {
+	  			$scope.drawRoute(newVal);
+	  			$scope.drawStop(newVal);
+				});
+
+				$scope.$watchCollection( 'vehicleLocation', function(newVal, oldVal) {
+	  			 $scope.drawVehicle(newVal);
+				});
 			}
 		}
 	}
